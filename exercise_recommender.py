5358eeecf7c1b3cce6a398d921475d799159b342
atayal73 <73803470+atayal73@users.noreply.github.com>
2020-11-22 13:45:29 -0800

    Add files via upload

--- exercise_recommender.py
+++ exercise_recommender.py
@@ -67,21 +67,56 @@ if fitness_goal.lower() == "A".lower():
     generate_cardio_recommendations(cardio_dict)
 
 elif fitness_goal.lower() == "B".lower():
-    upper_body_stretches = ['Cat-Camel Pose', 'Upper Trapezius (Neck Stretch)', 'Cross Body Shoulder Stretch'
-                            'Chest Stretch', 'Child''s Pose']
+    upper_body_stretches = ['Cat-Camel Pose', 'Upper Trapezius (Neck Stretch)', 'Overhead Triceps + Shoulders',
+                            'Cross Body Shoulder Stretch', 'Chest Stretch', 'Child''s Pose']
     lower_body_stretches = ['Hamstring Stretch', 'Butterfly Stretch', 'Hip Flexor Lunge Stretch',
                             'Pigeon Stretch', 'Lying Quad Stretch']
     upper_body_avg_volume = [551, 243, 436, 262, 231, 617]
-    upper_body_correction_factors = []
+    upper_body_cfs = [0.9, 0.76, 0.83, 0.69, 0.72, 0.88]
     lower_body_avg_volume = [779, 1290, 967, 1168.5, 2150]
-    lower_body_correction_factors = []
-    def calculate_stretching_scores(weight,upper_body_volume, lower_body_vol,difficulty):
-        upper_body__stretch_scores = []
+    lower_body_cfs = [0.78, 0.67, 0.73, 0.84, 0.69]
+
+    def calculate_stretching_scores(weight,upper_body_vol, upper_correction, lower_body_vol, lower_correction):
+        upper_body_stretch_scores = []
         lower_body_stretch_scores = []
-        stretch_score = 0;
-        for idx in range(len(upper_body_stretches)):
-            stretch_score = 2 * sqrt()
+        stretch_score = 0
+        # Calculate scores for upper body stretches, store in a list
+        for idx in range(len(upper_body_vol)):
+            difficulty = calculate_difficulty(age, fitness_level, upper_correction[idx])
+            stretch_score = 2/5 * upper_body_vol[idx] - 1/3 * weight + 5 * (20-difficulty)
+            upper_body_stretch_scores.append(stretch_score)
 
+        # Calculate scores for lower body stretches, store in a separate list
+        for j in range(len(lower_body_vol)):
+            difficulty = calculate_difficulty(age, fitness_level, lower_correction[j])
+            stretch_score = 2/5 * lower_body_vol[j] - 1/3 * weight + 5 * (20 - difficulty)
+            lower_body_stretch_scores.append(stretch_score)
 
+        return [upper_body_stretch_scores, lower_body_stretch_scores]
 
+    stretching_scores_lst = calculate_stretching_scores(weight, upper_body_avg_volume,
+                                                        upper_body_cfs, lower_body_avg_volume, lower_body_cfs)
+    # Create dictionaries to store stretches as keys and scores as values
+    upper_stretch_dict = {}
+    lower_stretch_dict = {}
+    for i in range(len(stretching_scores_lst[0])):
+        upper_stretch_dict.update({upper_body_stretches[i]: stretching_scores_lst[0][i]})
 
+    for j in range(len(stretching_scores_lst[1])):
+        lower_stretch_dict.update({lower_body_stretches[j]: stretching_scores_lst[1][j]})
+
+    def generate_stretch_recommendation(dict1,dict2):
+        # Sort the dictionary keys by the values
+        sorted_exercises1 = sorted(dict1, key=dict1.__getitem__)
+        sorted_exercises2 = sorted(dict2, key=dict2.__getitem__ )
+        # Print the recommendations
+        print('Based on your data we recommend ' + sorted_exercises1[-1] +
+              ' as your most effective upper body stretch \nand ' + sorted_exercises1[-2] +
+              ' for your second most effective upper body stretch\n')
+        print('For your lower body we recommend ' + sorted_exercises2[-1] +
+              ' as your most effective stretch \nand ' + sorted_exercises2[-2] +
+              ' as your second most effective stretch')
+
+    generate_stretch_recommendation(upper_stretch_dict, lower_stretch_dict)
+else:
+    upper_body_muscles = ['']
