2bb71560aa283c5c79685c1ddb476dd551e5defd
atayal73 <73803470+atayal73@users.noreply.github.com>
2020-12-01 16:57:34 -0800

    Add files via upload

--- public/exercise_recommender.py
+++ public/exercise_recommender.py
@@ -1,5 +1,6 @@
 # Insert copyright statement here
 from math import *
+
 # Gather various user inputs: age, fitness level, weight, and primary fitness goal
 age = int(input('Enter your age (as an integer) in years: '))
 while age < 15 or age > 75:
@@ -118,5 +119,55 @@ elif fitness_goal.lower() == "B".lower():
               ' as your second most effective stretch')
 
     generate_stretch_recommendation(upper_stretch_dict, lower_stretch_dict)
+
 else:
-    upper_body_muscles = ['']
+    upper_body_exercises = ['Pushups', 'Decline Pushups', 'Parallel Grip Pullup',
+                            'Side Plank', 'Tricep Dips', 'Lateral Plank Walk']
+    upper_body_avg_vol = [1276, 1276, 959, 817, 756, 1242]
+    upper_body_cfs = [0.65, 0.83, 0.79, 0.86, 0.73, 0.80]
+    lower_body_exercises = ['Bodyweight Squat', 'Reverse Lunges', 'Side Lunge', 'Box Jump',
+                            'Bulgarian Split Squats']
+    lower_body_avg_vol = [1828, 2150, 1912, 1828, 2310]
+    lower_body_cfs = [0.6, 0.73, 0.71, 0.78, 0.92]
+    def calculate_exercise_scores(weight,upper_body_vol, upper_correction, lower_body_vol, lower_correction):
+        upper_body_ex_scores = []
+        lower_body_ex_scores = []
+        exercise_score = 0
+        # Calculate scores for upper body stretches, store in a list
+        for idx in range(len(upper_body_vol)):
+            difficulty = calculate_difficulty(age, fitness_level, upper_correction[idx])
+            stretch_score = 2 / 5 * upper_body_vol[idx] - 1 / 3 * weight + 5 * (20 - difficulty)
+            upper_body_ex_scores.append(exercise_score)
+
+        # Calculate scores for lower body stretches, store in a separate list
+        for j in range(len(lower_body_vol)):
+            difficulty = calculate_difficulty(age, fitness_level, lower_correction[j])
+            stretch_score = 2 / 5 * lower_body_vol[j] - 1 / 3 * weight + 5 * (20 - difficulty)
+            lower_body_ex_scores.append(exercise_score)
+        return [upper_body_ex_scores, lower_body_ex_scores]
+
+    exercise_scores_lst = calculate_exercise_scores(weight,upper_body_avg_vol,
+                                                    upper_body_cfs, lower_body_avg_vol, lower_body_cfs)
+
+    # Create dictionaries to store exercises as keys and scores as values
+    upper_ex_dict = {}
+    lower_ex_dict = {}
+    for i in range(len(exercise_scores_lst[0])):
+        upper_ex_dict.update({upper_body_exercises[i]: exercise_scores_lst[0][i]})
+
+    for j in range(len(exercise_scores_lst[1])):
+        lower_ex_dict.update({lower_body_exercises[j]: exercise_scores_lst[1][j]})
+
+    def generate_exercise_recommendations(dict1, dict2):
+        # Sort the dictionary keys by the values
+        sorted_exercises1 = sorted(dict1, key=dict1.__getitem__)
+        sorted_exercises2 = sorted(dict2, key=dict2.__getitem__)
+        # Print the recommendations
+        print('Based on your data we recommend ' + sorted_exercises1[-1] +
+              ' as your most effective upper body exercise \nand ' + sorted_exercises1[-2] +
+              ' for your second most effective upper body exercise\n')
+        print('For your lower body we recommend ' + sorted_exercises2[-1] +
+              ' as your most effective exercise \nand ' + sorted_exercises2[-2] +
+              ' as your second most effective exercise')
+
+    generate_exercise_recommendations(upper_ex_dict, lower_ex_dict)
