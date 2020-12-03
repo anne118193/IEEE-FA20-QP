d22181aeecaa7c5beb41cca5021d01b02fe0c9c1
anne118193 <65373942+anne118193@users.noreply.github.com>
2020-12-03 11:11:44 -0800

    Add files via upload

---
+++ public/init.js
@@ -0,0 +1,13 @@
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+var firebaseConfig = {
+  apiKey: "V4sPwQ.6UspKg",
+  authDomain: "ieee-fall-20-qp.firebaseapp.com",
+  databaseURL: "https://ieee-fall-20-qp.firebaseio.com",
+  projectId: "ieee-fall-20-qp",
+  storageBucket: "ieee-fall-20-qp.appspot.com",
+  messagingSenderId: "122142811344",
+  appId: "1:122142811344:web:fcce08f6a67c4e98aeaad5",
+  //measurementId: "G-MEASUREMENT_ID",
+};
+
+export {firebaseConfig};
