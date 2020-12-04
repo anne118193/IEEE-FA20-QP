718fcf700576807b3127a0a8dafb5cb0994da85a
pmaddire <74743711+pmaddire@users.noreply.github.com>
2020-12-03 18:09:19 -0800

    Update main.js

--- public/EMC/js/main.js
+++ public/EMC/js/main.js
@@ -123,6 +123,11 @@ firebase.auth().signInWithEmailAndPassword(email, password)
     var errorCode = error.code;
     var errorMessage = error.message;
   });
+firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}).catch(function(error) {
+  // An error happened.
+});
 
  
 /*
